4f71d3759a8dcc8e11fae72eafdf052139a91388
mcpeapsUnterstrischHD <mcpeaps_HD@outlook.com>
2021-12-02 17:09:14 +0100

    Update temp.java

--- PAS/Schleifen/loops/temp.java
+++ PAS/Schleifen/loops/temp.java
@@ -1,13 +1,11 @@
-public static void temp() {
-    
+public class Temp {
+    public static void main(String[] args) {
+        int zahl;
+        zahl = 1;
+        while (zahl <= 2) {
 
-public static void main(String[] args) {
-
-int zahl;
-zahl = 1;
-    
-    while (zahl < 2) {
-        System.out.println(zahl);
-        zahl++;
-    }   
-}}
+            System.out.println(zahl);
+            zahl++;
+        }
+    }
+}
