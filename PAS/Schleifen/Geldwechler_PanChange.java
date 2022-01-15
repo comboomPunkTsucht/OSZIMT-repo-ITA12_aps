abb20af913d2be261b62b48454269822a26ee74d
mcpeapsunterstrichHD <mcpeaps_HD@outlook.com>
2022-01-15 15:19:36 +0100

    ..

--- PAS/Schleifen/Geldwechler_PanChange.java
+++ PAS/Schleifen/Geldwechler_PanChange.java
@@ -1,95 +1,93 @@
+import java.util.*;
 
 public class Geldwechler_PanChange {
 
-    import java.util.*;
-public static void main(String[] args) {
-    Scanner auswahl_scan = new Scanner(System.in);
-    Scanner betrag_scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner auswahl_scan = new Scanner(System.in);
+        Scanner betrag_scan = new Scanner(System.in);
 
+        float usdeur = 0.8739F;// stand 13.1.22
+        float eurusd = 1.1442F;// stand 13.1.22
+        float betrag = 0.00F;
+        float betrag_ct = 0.00F;
+        float betrag_e = 0.00F;
+        float betrag_e_e = 0.00F;
 
-
-    float usd0eur = 0.8739F;//stand 13.1.22
-    float eurusd = 1.1442F;//stand 13.1.22
-
-    System.out.println("**************************************************");
-    System.out.println("*              Whähle Die Richtung               *");
-    System.out.println("*                  [U]SD --> EUR                 *");
-    System.out.println("*                  [E]UR --> USD                 *");
-    System.out.println("**************************************************");
-    System.out.print("=> ");
-
-    char auswahl = auswahl_scan.next().charAt(0);
-
-    switch (auswahl) {
-        case 'E':
         System.out.println("**************************************************");
-        System.out.println("*          Geben Sie ihren Betrag ein            *");
+        System.out.println("*              Whähle Die Richtung               *");
+        System.out.println("*                  [U]SD --> EUR                 *");
+        System.out.println("*                  [E]UR --> USD                 *");
         System.out.println("**************************************************");
         System.out.print("=> ");
 
-        float betrag = betrag_scan.nextFloat();
+        char auswahl = auswahl_scan.next().charAt(0);
 
-        betrag = Math.round(2);
-        betrag = betrag * eurusd;
-        betrag = (99*100) / betrag;
-        float betrag_ct = betrag * 100;
-
-            break;
-        
-            case 'e':
-            System.out.println("**************************************************");
-            System.out.println("*          Geben Sie ihren Betrag ein            *");
-            System.out.println("**************************************************");
-            System.out.print("=> ");
-    
-            float betrag = betrag_scan.nextFloat();
-    
-            betrag = Math.round(2);
-            betrag = betrag * eurusd;
-            betrag = (99*100) / betrag;
-            float betrag_ct = betrag * 100;
-    
-                break;
-            
-                case 'U':
+        switch (auswahl) {
+            case 'E':
                 System.out.println("**************************************************");
                 System.out.println("*          Geben Sie ihren Betrag ein            *");
                 System.out.println("**************************************************");
                 System.out.print("=> ");
-        
-                float betrag = betrag_scan.nextFloat();
-        
-                betrag = Math.round(2);
+
+                betrag = betrag_scan.nextFloat();
+
                 betrag = betrag * usdeur;
-                betrag = (99*100) / betrag;
-                float betrag_ct = betrag * 100;
-        
-                    break;
-                
-                    case 'u':
-                    System.out.println("**************************************************");
-                    System.out.println("*          Geben Sie ihren Betrag ein            *");
-                    System.out.println("**************************************************");
-                    System.out.print("=> ");
-            
-                    float betrag = betrag_scan.nextFloat();
-            
-                    betrag = Math.round(2);
-                    betrag = betrag * usdeur;
-                    betrag = (99*100) / betrag;
-                    float betrag_ct = betrag * 100;
-            
-                        break;
-                
-            
-        
-    
-        default:
-            break;
-    }
+                betrag_ct = betrag * 100;
+                betrag_e = (99F * betrag_ct) / 100F;
+                betrag_e_e = betrag_e / 100F;
 
+                break;
 
-    System.out.println(betrag);
+            case 'e':
+                System.out.println("**************************************************");
+                System.out.println("*          Geben Sie ihren Betrag ein            *");
+                System.out.println("**************************************************");
+                System.out.print("=> ");
+
+                betrag = betrag_scan.nextFloat();
+
+                betrag = betrag * usdeur;
+                betrag_ct = betrag * 100;
+                betrag_e = (99F * betrag_ct) / 100F;
+                betrag_e_e = betrag_e / 100F;
+
+                break;
+
+            case 'U':
+                System.out.println("**************************************************");
+                System.out.println("*          Geben Sie ihren Betrag ein            *");
+                System.out.println("**************************************************");
+                System.out.print("=> ");
+
+                betrag = betrag_scan.nextFloat();
+
+                betrag = betrag * usdeur;
+                betrag_ct = betrag * 100;
+                betrag_e = (99F * betrag_ct) / 100F;
+                betrag_e_e = betrag_e / 100F;
+
+                break;
+
+            case 'u':
+                System.out.println("**************************************************");
+                System.out.println("*          Geben Sie ihren Betrag ein            *");
+                System.out.println("**************************************************");
+                System.out.print("=> ");
+
+                betrag = betrag_scan.nextFloat();
+
+                betrag = betrag * usdeur;
+                betrag_ct = betrag * 100;
+                betrag_e = (99F * betrag_ct) / 100F;
+                betrag_e_e = betrag_e / 100F;
+
+                break;
+
+            default:
+                break;
+        }
+
+        System.out.println(betrag_e_e);
 
     }
 }
