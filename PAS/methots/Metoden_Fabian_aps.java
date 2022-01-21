42f7ab33d778aa85bbeaee0e422164fcbe172ead
mcpeapsunterstrichHD <mcpeaps_HD@outlook.com>
2022-01-21 09:12:02 +0100

    +

---
+++ PAS/methots/Metoden_Fabian_aps.java
@@ -0,0 +1,12 @@
+public class Metoden_Fabian_aps {
+
+    public static int summe(int n1, int n2) {
+        // Liefert die Summe von n1 und n2 als Ergebnis.
+        int sum = n1 + n2;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        summe(6, 8);
+    }
+}
