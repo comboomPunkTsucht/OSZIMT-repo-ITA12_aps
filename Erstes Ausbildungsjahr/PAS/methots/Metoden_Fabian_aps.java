c00e35e37366d80eede15aae253a15869a17f56a
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2022-01-27 14:32:00 +0100

    *

--- Erstes Ausbildungsjahr/PAS/methots/Metoden_Fabian_aps.java
+++ Erstes Ausbildungsjahr/PAS/methots/Metoden_Fabian_aps.java
@@ -6,7 +6,97 @@ public class Metoden_Fabian_aps {
         return sum;
     }
 
+    public static void gibAus(int n1, int n2, int n3) {
+        String sternchen = "******************";
+        System.out.println(sternchen);
+        System.out.println("*   1.Wert: " + n1 + "    *");
+        System.out.println("*   2.Wert: " + n2 + "    *");
+        System.out.println("*   3.Wert: " + n3 + "    *");
+        System.out.println(sternchen);
+    }
+
+    public static void gibaus(int line) {
+        for (int i = 0; i == line; i++) {
+            System.out.print("*-*");
+        }
+
+    }
+
+    public static int quadrierung(int b) {
+        int quadriert = b * b;
+        return quadriert;
+    }
+
+    public static long binearrechner(int n) {
+        long b = 2;
+        if (n < 0) {
+            b = 1;
+        } else {
+            for (int i = 1; i != n; i++) {
+                b = b * 2;
+            }
+        }
+
+        return b;
+    }
+
+    public static long binearrechner2(long b, int n) {
+        if (n < 0) {
+            b = 1;
+        } else {
+            for (int i = 1; i != n; i++) {
+                b = b * 2;
+            }
+        }
+
+        return b;
+    }
+
+    public static double pytagoras(double a, double b) {
+        double c = Math.sqrt((a * a) + (b * b));
+        return c;
+    }
+
     public static void main(String[] args) {
-        System.out.println(summe(6,8))
+        String sternchen = "*****************************************************************************************************************************";
+        int aufgabe = 1;
+        System.out.println(sternchen);
+        System.out.println("Aufgabe " + aufgabe);
+        System.out.println(sternchen);
+        System.out.println(summe(6, 8));
+        aufgabe++;
+        System.out.println(sternchen);
+        System.out.println("Aufgabe " + aufgabe);
+        System.out.println(sternchen);
+        gibAus(1, 2, 3);
+        aufgabe++;
+        System.out.println(sternchen);
+        System.out.println("Aufgabe " + aufgabe);
+        System.out.println(sternchen);
+        gibaus(5);
+        aufgabe++;
+        System.out.println(sternchen);
+        System.out.println("Aufgabe " + aufgabe);
+        System.out.println(sternchen);
+        System.out.println(quadrierung(aufgabe));
+        aufgabe++;
+        System.out.println(sternchen);
+        System.out.println("Aufgabe " + aufgabe);
+        System.out.println(sternchen);
+        System.out.println(binearrechner(aufgabe));
+        aufgabe++;
+        System.out.println(sternchen);
+        System.out.println("Aufgabe " + aufgabe);
+        System.out.println(sternchen);
+        System.out.println(binearrechner2(6, aufgabe));
+        aufgabe++;
+        System.out.println(sternchen);
+        System.out.println("Aufgabe " + aufgabe);
+        System.out.println(sternchen);
+        System.out.println(pytagoras(6, aufgabe));
+        aufgabe++;
+        System.out.println(sternchen);
+        System.out.println("Aufgabe " + aufgabe);
+        System.out.println(sternchen);
     }
 }
