979b5f83d80d03381502395b47dfe55a4a28076b
mcpeaps_HD <mcpeaps_HD@outlook.com>
2022-04-07 13:43:11 +0200

    *

--- Erstes Ausbildungsjahr/PAS/arrays/D4dot4.java
+++ Erstes Ausbildungsjahr/PAS/arrays/D4dot4.java
@@ -6,9 +6,24 @@ public class D4dot4 {
     char[] z = { 'a', '#' };
     String[] v = { "Ar", "ra", "y" };
 
+    System.out.println("A1");
     System.out.println(x[0]);
     System.out.println(y[3]);
     System.out.println(z[1]);
     System.out.println(v[0]);
+    array_a2()
+  }
+
+  public static void array_a2() {
+    int[] x;
+    x[0] = 12;
+    x[1] = 11;
+    x[2] = -1;
+    System.out.println("A2");
+    System.out.println("  | intArray");
+    System.out.println("----------------");
+    System.out.println("0 | " + x[0]);
+    System.out.println("1 | " + x[1]);
+    System.out.println("2 | " + x[2]);
   }
 }
