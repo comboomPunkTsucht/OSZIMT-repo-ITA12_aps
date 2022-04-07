2570c9f4017ffb0b12b31dd39a5ceb8e67759dc4
mcpeaps_HD <mcpeaps_HD@outlook.com>
2022-04-07 13:08:00 +0200

    *

---
+++ Erstes Ausbildungsjahr/arrays/D4dot4.java
@@ -0,0 +1,13 @@
+public class D4dot4 {
+    public static void main(String[] args) {
+        int[] x = {12,11,-1};
+        double[] y = {1.3,1.4,-12.3,2.23};
+        char[] z = {'a','#'}
+        String[] v = {"Ar","ra","y"};
+
+        System.out.println(x[0]);
+        System.out.println(y[3]);
+        System.out.println(z[1]);
+        System.out.println(v[0]);
+
+}
