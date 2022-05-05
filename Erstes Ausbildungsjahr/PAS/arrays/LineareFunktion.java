c59633b072d0affbfc0856aa9eb06fdc947c7780
mcpeaps_HD <mcpeaps_HD@outlook.com>
2022-05-05 13:39:10 +0200

    *

--- Erstes Ausbildungsjahr/PAS/arrays/LineareFunktion.java
+++ Erstes Ausbildungsjahr/PAS/arrays/LineareFunktion.java
@@ -1,1 +1,19 @@
-public class LineareFunktion {}
+import java.util.Scanner;
+public class LineareFunktion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double funktion[] = new double[21];
+        double m;
+        double n;
+
+        System.out.println("Bitte geben Sie die Steigung ein: ");
+        m = scanner.nextDouble();
+        System.out.println("Bitte geben Sie den y-Achsenabschnit ein: ");
+        n = scanner.nextDouble();
+
+        for (int x = x < funktion.lengint x++) {
+            double funktion[x] = m * x + n;
+            
+        }
+    }
+}
