9a1d680e0b57d960aed8ec7ee8ae5e2151d720b8
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-01-18 08:38:22 +0100

    .

--- Zweites Ausbildungsjahr/PAS/OO-3/APÜ/APÜ/MyWorld.java
+++ Zweites Ausbildungsjahr/PAS/OO-3/APÜ/APÜ/MyWorld.java
@@ -13,28 +13,28 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
-    {    
-    M monitor = new M();
-
-    monitor.setName("M1");
-    monitor.setResolution("3840×2160");
-    monitor.setSizeInInch(27.00);
-    Computer computer = new Computer("HaaPee007", "Zintel x3", 16.00, monitor);
-    monitor.setC1(computer);
-
-    System.out.println(monitor.getName());
-    System.out.println(monitor.getResolution());
-    System.out.println(monitor.getSizeInInch());
-    System.out.println(monitor.getC1());
-    System.out.println(monitor.getC1().getName());
-    System.out.println(monitor.getC1().getCPU());
-    System.out.println(monitor.getC1().getRAM());
-    System.out.println(monitor.getC1().getM1());
-    System.out.println(monitor.getC1().getM1().getName());
-
-    computer.rechnen();
-    monitor.print();
-        super(600, 400, 1); 
+    public MyWorld(){
+        super(600, 400, 1);
+        M monitor = new M();
+    
+        monitor.setName("M1");
+        monitor.setResolution("3840×2160");
+        monitor.setSizeInInch(27.00);
+        Computer computer = new Computer("HaaPee007", "Zintel x3", 16.00, monitor);
+        monitor.setC1(computer);
+    
+        System.out.println(monitor.getName());
+        System.out.println(monitor.getResolution());
+        System.out.println(monitor.getSizeInInch());
+        System.out.println(monitor.getC1());
+        System.out.println(monitor.getC1().getName());
+        System.out.println(monitor.getC1().getCPU());
+        System.out.println(monitor.getC1().getRAM());
+        System.out.println(monitor.getC1().getM1());
+        System.out.println(monitor.getC1().getM1().getName());
+    
+        computer.rechnen();
+        monitor.print();
+         
     }
 }
