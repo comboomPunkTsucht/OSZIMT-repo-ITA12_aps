51de9151798a67936cbfcbce0b8a65fdb7d29a68
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-01-23 08:06:37 +0100

    .

---
+++ Zweites Ausbildungsjahr/PAS/OO-4/Greenfoot-Spieleprojekt/PixelCoder/MyWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot und MouseInfo)
+
+/**
+ * Ergänzen Sie hier eine Beschreibung für die Klasse MyWorld.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Konstruktor für Objekte der Klasse MyWorld
+     * 
+     */
+    public MyWorld()
+    {    
+        // Erstellt eine neue Welt mit 600x400 Zellen und einer Zell-Größe von 1x1 Pixeln.
+        super(600, 400, 1); 
+    }
+}
