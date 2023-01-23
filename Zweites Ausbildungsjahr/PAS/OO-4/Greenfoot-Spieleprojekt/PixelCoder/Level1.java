40e49defda51c1508553e5c99ab450adbb1582c4
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-01-23 09:23:36 +0100

    .

--- Zweites Ausbildungsjahr/PAS/OO-4/Greenfoot-Spieleprojekt/PixelCoder/Level1.java
+++ Zweites Ausbildungsjahr/PAS/OO-4/Greenfoot-Spieleprojekt/PixelCoder/Level1.java
@@ -6,19 +6,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot und MouseInfo)
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class MyWorld extends World
+public class Level1 extends World
 {
 
     /**
      * Konstruktor für Objekte der Klasse MyWorld
      * 
      */
-    public MyWorld()
+    public Level1()
     {    
         // Erstellt eine neue Welt mit 600x400 Zellen und einer Zell-Größe von 1x1 Pixeln.
         super(1280, 720, 1); 
         
-        Player1 p1 = new Player1(600, 420);
-        Player2 p2 = new Player2(600, 420);
+        Player1 p1 = new Player1();
+        Player2 p2 = new Player2();
+        
+        addObject(p1, 640, 360);
+        addObject(p2, 640, 360);
     }
 }
--- Zweites Ausbildungsjahr/PAS/OO-4/Greenfoot-Spieleprojekt/PixelCoder/Player1.java
+++ Zweites Ausbildungsjahr/PAS/OO-4/Greenfoot-Spieleprojekt/PixelCoder/Player1.java
@@ -8,19 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot und MouseInfo)
  */
 public class Player1 extends Actor   
 {
-    private int x = 0;
-    private int y = 0;
-    
-    
     Player1(){}
-    Player1(int x, int y){
-        this.x = x;
-        this.y = y;
-        setLocation(this.x, this.y);
-    
-    }
     /**
-     * Act - tut, was auch immer Player1 tun will. Diese Methode wird aufgerufen, 
+     * Act - tut, was auch immer Player2 tun will. Diese Methode wird aufgerufen, 
      * sobald der 'Act' oder 'Run' Button in der Umgebung angeklickt werden. 
      */
     public void act() 
@@ -28,9 +18,16 @@ public class Player1 extends Actor
         this.movement();
     }
     public void movement() {
-        if (Greenfoot.isKeyDown("Up")){
-            this.y = getY() - 5;
-            setLocation(this.x, this.y);
-        }
+        if (Greenfoot.isKeyDown("W")){
+            setLocation(getX(), (getY() - 5));
+            }else if (Greenfoot.isKeyDown("D")){
+                setLocation((getX() + 5), getY());
+            }else if (Greenfoot.isKeyDown("A")){
+                setLocation((getX() - 5), getY());
+            }else if (Greenfoot.isKeyDown("D") && Greenfoot.isKeyDown("W")){
+                setLocation((getX() + 5 ), (getY() - 5));
+            }else if (Greenfoot.isKeyDown("A") && Greenfoot.isKeyDown("W")){
+                setLocation((getX() - 5 ), (getY() - 5));
+            }else{setLocation(getX(), (getY() + 5));};
     }
 }
