d7ce7f8af86968f98148f57476ddbb92d65baadf
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-05-24 18:49:58 +0200

    .

---
+++ Zweites Ausbildungsjahr/PAS/OO-7/LoginWindow/src/UMWindow.java
@@ -0,0 +1,61 @@
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class UMWindow {
+
+    private JFrame frmOszImtKickers;
+    private JPanel panel;
+    private String nameOfUser;
+    private JLabel lblNewLabel;
+    ImageIcon icon = new ImageIcon("icon.png");
+
+    /**
+     * Create the application.
+     */
+    public UMWindow(String name) {
+	this.nameOfUser = name;
+	initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+	frmOszImtKickers = new JFrame();
+	frmOszImtKickers.setIconImage(Toolkit.getDefaultToolkit().getImage("icon.png"));
+	frmOszImtKickers.setTitle("OSZ IMT Kickers - User Management");
+	frmOszImtKickers.setBounds(100, 100, 450, 102);
+	frmOszImtKickers.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frmOszImtKickers.getContentPane().setLayout(null);
+	frmOszImtKickers.getContentPane().add(getPanel());
+	frmOszImtKickers.setVisible(true);
+    }
+
+    private JPanel getPanel() {
+	if (panel == null) {
+	    panel = new JPanel();
+	    panel.setBounds(0, 0, 450, 74);
+	    panel.setName("panel");
+	    panel.setLayout(new GridLayout(1, 1));
+	    panel.add(getLblNewLabel());
+	}
+	return panel;
+    }
+
+    private JLabel getLblNewLabel() {
+	if (lblNewLabel == null) {
+	    lblNewLabel = new JLabel("Hallo, " + nameOfUser);
+	    lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+	    lblNewLabel.setName("lblNewLabel");
+	}
+	return lblNewLabel;
+    }
+}
