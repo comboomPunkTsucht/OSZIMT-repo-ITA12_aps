1b6c3d42be4890e81552a427296236f5d7795eda
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-05-25 06:40:02 +0200

    .

--- Zweites Ausbildungsjahr/PAS/OO-7/LoginWindow/src/LoginWindow.java
+++ Zweites Ausbildungsjahr/PAS/OO-7/LoginWindow/src/LoginWindow.java
@@ -154,13 +154,13 @@ public class LoginWindow {
 		    // TODO Auto-generated method stub
 		    if (userManagement.exist(tfd_Username.getText(), String.valueOf(tfd_Pwd.getPassword())) == true) {
 			frame.dispose();
-			new UMWindow(tfd_Username.getText());
 			JOptionPane.showOptionDialog(null, "Login in ........", "Login Sucsesfull",
 				JOptionPane.CLOSED_OPTION, JOptionPane.INFORMATION_MESSAGE, icon, null, 0);
+			new UMWindow(tfd_Username.getText());
 
 		    } else {
 			JOptionPane.showOptionDialog(null, "username or passwor are wrong", "Login unsucsesfull",
-				JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE, icon, null, 0);
+				JOptionPane.CLOSED_OPTION, JOptionPane.ERROR_MESSAGE, null, null, 0);
 		    }
 		}
 	    });
