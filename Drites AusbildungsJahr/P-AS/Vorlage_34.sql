e97b08cefa16b07b9434f16cca1c35679a947f2f
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-09-29 14:23:20 +0200

    .

---
+++ Drites AusbildungsJahr/P-AS/Vorlage_34.sql
@@ -0,0 +1,40 @@
+-- ---------------------------------------------------------------------- --
+-- Task 3.4 - Weitere Aggregationsfunktionen                              --
+-- ---------------------------------------------------------------------- --
+
+-- a) Wie viele Gerätetypen haben wir eigentlich pro Hersteller im System 
+--    eingepflegt?
+
+-- b) Wie viele Ports sind an den einzelnen Switches laut System eigentlich 
+--    belegt?
+
+-- c) Und wie viele Ports haben eigentlich unsere PoE-fähigen Switches
+--    insgesamt?
+
+-- d) Schauen Sie mal bitte nach, wie viele IP-Telefone von Cusco
+--    (Gerätetypen 92 und 93) wir momentan aufgestellt haben.
+
+-- e) Für die Versicherung muss ich wissen, wie teuer jeweils das teuerste
+--    Gerät in den verschiedenen Gebäuden ist, und wie viel die Geräte jeweils
+--    insgesamt sind, jeweils sortiert von niedrig bis hoch.
+
+-- f) Und dann war noch die Frage, in welchen Räumen der Durchschnittspreis
+--    der Geräte über 500 € liegt, dort bräuchten wir dann eventuell ein
+--    neues Schloss an der Tür. 
+
+-- g) Für die Bilanz müssen wissen, wie groß der Gesamtwert aller Geräte pro
+--    Gerätetyp im Bestand ist, absteigend nach Gesamtwert sortiert.
+
+-- h) Fürs Finanzamt brauchen wir die Anzahl der gekauften Geräte pro Jahr.
+--    (Tipp: verwenden Sie die YEAR()-Funktion in der GROUP BY-Anweisung).
+
+-- i) Ich wüsste ganz gerne, wie viele Geräte im Laufe des aktuellen Jahres
+--    aus der Garantie fallen, und wieviel diese Geräte insgesamt wert sind.
+
+-- j) Und von welchen Geräteypen haben wir eigentlich mehr als 30 Stück im
+--    Bestand gelistet?
+
+-- k) Es gab eine Beschwerde, dass einige Kolleginnen und Kollegen deutlich
+--    mehr Räume betreuen müssen, als andere. Können Sie eine Liste aller
+--    Ansprechpartner ausgebeben, die im Hauptgebäude mehr als 5 Räume
+--    betreuen müssen, absteigend sortiert nach der Anzahl der Räume?
