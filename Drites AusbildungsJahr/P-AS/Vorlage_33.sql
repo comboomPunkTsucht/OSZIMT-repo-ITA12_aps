621dba8749178f956c3f83cd5d2d88d95269c583
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-09-29 14:10:10 +0200

    .

---
+++ Drites AusbildungsJahr/P-AS/Vorlage_33.sql
@@ -0,0 +1,25 @@
+-- ---------------------------------------------------------------------- --
+-- Task 3.3 - Anfragen für die Kita                                       --
+-- ---------------------------------------------------------------------- --
+
+-- 1.)  Wie viele Kinder gehen in den Kindergarten?
+
+-- 2.)  Wann hat das jüngste und das älteste Kind Geburtstag?
+
+-- 3.)  Welche Spendensumme steht der Kita insgesamt zur Verfügung?
+
+-- 4.)  Wie viele Jungen und Mädchen gibt es? (GROUP BY)
+
+-- 5.)  Wie viele Kinder hat jeder Betreuer?
+
+-- 6.)  Welche Betreuer sind für mehr als drei Kindern zuständig? (HAVING).
+
+-- 7.)  Wie viele Jungen und Mädchen hat jeder Betreuer?
+
+-- 8.)  Wie viele Kinder entfallen auf ein Geburtsjahr?
+
+-- 9.)  Welche monatliche Spendensumme hat jeder Betreuer zur Verfügung?
+
+-- 10.) Wie ist die höchste und niedrigste Elternspende pro Betreuer?
+
+-- 11.) Wie hoch ist die durchschnittliche Elternspende pro Betreuer?
