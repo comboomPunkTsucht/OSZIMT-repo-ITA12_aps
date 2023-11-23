1994eaec39e75fb402c25e816613bb671aa9239c
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-11-23 11:55:03 +0100

    ..

--- Drites AusbildungsJahr/P-AS/Database_create/fitnesscenter.sql
+++ Drites AusbildungsJahr/P-AS/Database_create/fitnesscenter.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS T_kunden (
     kuvornahme VARCHAR(255),
     kuplz INT,
     kuort VARCHAR(255),
-    kutelefon INT,
+    kutelefon VARCHAR(255),
     kugeburtsdatum DATE,
     kuemail VARCHAR(255),
     kumerk VARCHAR(255)
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS T_trainer (
     trstrasse VARCHAR(255),
     trplz INT,
     trort VARCHAR(255),
-    trtelefon INT,
+    trtelefon VARCHAR(255),
     trgeburtsdatum DATE,
     tremail VARCHAR(255),
     trquali VARCHAR(255)
@@ -59,10 +59,10 @@ CREATE TABLE IF NOT EXISTS T_teilnahme (
 
 -- Fügen Sie Daten in die Tabelle 'kunden' ein
 INSERT INTO T_kunden VALUES
-(1001, 'Herr', 'Pflaume', 'Renè', 10781, 'Berlin', 9588745, '1980-07-24', 'pglaume@pflaume.net', 'Liebt Ausdauerkurse'),
-(1202, 'Frau', 'Müller', 'Anja', 10435, 'Potsdam', 3411833, '1983-06-30', 'anja@provider.de', 'Wasserratte'),
-(1210, 'Frau', 'Dorfmann', 'Yvonne', 26133, 'Oldenburg', 02345342489, '1981-08-07', 'yvonne@provider.de', ''),
-(1213, 'Herr', 'Krokowski', 'Timo', 10559, 'Berlin', 3944781, '1980-03-23', 'timo.de', '');
+(1001, 'Herr', 'Pflaume', 'Renè', 10781, 'Berlin', '9588745', '1980-07-24', 'pglaume@pflaume.net', 'Liebt Ausdauerkurse'),
+(1202, 'Frau', 'Müller', 'Anja', 10435, 'Potsdam', '3411833', '1983-06-30', 'anja@provider.de', 'Wasserratte'),
+(1210, 'Frau', 'Dorfmann', 'Yvonne', 26133, 'Oldenburg', '02345342489', '1981-08-07', 'yvonne@provider.de', ''),
+(1213, 'Herr', 'Krokowski', 'Timo', 10559, 'Berlin', '3944781', '1980-03-23', 'timo.de', '');
 
 -- Fügen Sie Daten in die Tabelle 'kurse' ein
 INSERT INTO T_kurse VALUES
@@ -71,14 +71,14 @@ INSERT INTO T_kurse VALUES
 
 -- Fügen Sie Daten in die Tabelle 'trainer' ein
 INSERT INTO T_trainer VALUES 
-(11221, 'Herr', 'Pflaume', 'Renè', 'Pallasstr. 5', 10781, 'Berlin', 9588745, '1980-07-24', 'pglaume@fantasie.com', 'Diplom-Sportlehrer'),
-(11422, 'Frau', 'Dietz', 'Anja', 'Kalkscheuenstr. 22', 10435, 'Berlin',3411833,'1983-06-30', 'pglaume@fantasie.com', 'Diplom-Sportlehrer');
+(11221, 'Herr', 'Pflaume', 'Renè', 'Pallasstr. 5', 10781, 'Berlin', '9588745', '1980-07-24', 'pglaume@fantasie.com', 'Diplom-Sportlehrer'),
+(11422, 'Frau', 'Dietz', 'Anja', 'Kalkscheuenstr. 22', 10435, 'Berlin','3411833','1983-06-30', 'pglaume@fantasie.com', 'Diplom-Sportlehrer');
 
 -- Fügen Sie Daten in die Tabelle 'teilnahme' ein
 INSERT INTO T_teilnahme VALUES 
-(2, 102, 6163, '2017-10-02', '0000-00-00'),
-(3, 104, 6166, '2017-10-02', '0000-00-00'),
-(4, 100, 6163, '2017-10-02', '0000-00-00'),
-(5, 107, 6166, '2017-10-02', '0000-00-00'),
-(6, 105, 4946, '2017-10-02', '0000-00-00');
+(2, 102, 6163, '2017-10-02', NULL),
+(3, 104, 6166, '2017-10-02', NULL),
+(4, 100, 6163, '2017-10-02', NULL),
+(5, 107, 6166, '2017-10-02', NULL),
+(6, 105, 4946, '2017-10-02', NULL);
 
